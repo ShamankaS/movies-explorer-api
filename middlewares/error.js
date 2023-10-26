213e8610f5c2891a4c57c1dc09e5463c21623012
Tatiana <115077020+ShamankaS@users.noreply.github.com>
2023-10-26 18:03:17 +0300

    Level 1 (#9)
    * feat: models done
    
    * feat: routes and controllers done
    
    * feat: authentication and authorization done
    
    * feat: logging done
    
    * README.md update
    
    * feat: cors added
    
    * fixes
    
    * Update README.md
    
    * fixes after review
    
    * fixes for frontend part

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,6 +4,7 @@ module.exports = (error, req, res, next) => {
   const { statusCode = 500, message } = error;
   res.status(statusCode).send({
     message: statusCode === 500 ? UNKNOWN_ERROR : message,
+    statusCode,
   });
   next();
 };
